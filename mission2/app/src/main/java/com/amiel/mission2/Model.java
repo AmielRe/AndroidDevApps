5ac2af6a8e26bfce47d78976d4e75e55474050ad
Omer <omerarviv801@gmail.com>
2022-12-06 21:30:38 +0200

    omer commit

--- mission2/app/src/main/java/com/amiel/mission2/Model.java
+++ mission2/app/src/main/java/com/amiel/mission2/Model.java
@@ -12,9 +12,21 @@ public class Model {
     }
 
     private Model(){
-        for(int i=0;i<20;i++){
+        /*for(int i=0;i<20;i++){
             addStudent(new Student("name " + i,""+i,"",false));
-        }
+        }*/
+
+
+        addStudent(new Student("Aharon", "205967841","",false,"0528886341","Afula"));
+        addStudent(new Student("Barney", "205967842", "" , false, "0528886342", "Bat Yam"));
+        addStudent(new Student("Charlie","205967843", "", false,"0528886343","Chicago"));
+        addStudent(new Student("David","205967844","",false,"0528886344","Dimona"));
+        addStudent(new Student("Edgar","205967845","",false,"0528886345","Eilat"));
+        addStudent(new Student("Felix","205967846","",false,"0528886346","Frankfurt"));
+        addStudent(new Student("Gabriel","205967847","",false,"0528886347","Givatayim"));
+        addStudent(new Student("Henry","205967848","",false,"0528886348","Haifa"));
+        addStudent(new Student("Isaac","205967849","",false,"0528886349","Istanbul"));
+
     }
 
     List<Student> data = new LinkedList<>();
--- mission2/app/src/main/java/com/amiel/mission2/StudentDetailsActivity.java
+++ mission2/app/src/main/java/com/amiel/mission2/StudentDetailsActivity.java
@@ -1,14 +1,78 @@
 package com.amiel.mission2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class StudentDetailsActivity extends AppCompatActivity {
+    Student student;
+    int numberData;
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        recreate();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_details);
+
+        // get the student number from intent extra info
+        Intent intent = getIntent();
+        numberData = intent.getIntExtra("name", 0);
+        student=Model.instance().data.get(numberData);
+
+
+        TextView nameTv = findViewById(R.id.studentdetails_name);
+        TextView idTv = findViewById(R.id.studentdetails_id);
+        TextView phoneTv = findViewById(R.id.studentdetails_phone);
+        TextView addressTv = findViewById(R.id.studentdetails_address);
+        CheckBox cb = findViewById(R.id.studentdetails_checkbox);
+        Button editBtn = findViewById(R.id.studentdetails_editbtn);
+
+        nameTv.setText("name : "+student.name);
+        idTv.setText("id : "+student.id);
+        phoneTv.setText("phone : "+student.phone);
+        addressTv.setText("address : "+student.address);
+        if(student.cb)
+            cb.setChecked(true);
+        else
+            cb.setChecked(false);
+
+
+        /*cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                student.cb=!student.cb;
+            }
+        });*/
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), StudentEditActivity.class);
+                intent.putExtra("name",numberData);
+                startActivity(intent);
+            }
+        });
+
+
+        //need to fix for each specific image in next missions
+        ImageView imageView = findViewById(R.id.studentdetails_img);
+        imageView.setImageResource(R.drawable.avatar);
     }
 }
